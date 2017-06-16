72d118372696a7ce832c4656e8b305340406d183
woldena <wolden.andre@gmail.com>
2017-06-16 10:50:27 +0200

    copied stuff from squash2 tull

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :games
+  resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
+  get '/stats', to: 'static_pages#stats'
 
 end
